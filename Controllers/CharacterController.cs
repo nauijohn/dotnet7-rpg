d77c5001fce261b6c59b90e39b78f682d00429dc
nauijohn <85679013+nauijohn@users.noreply.github.com>
2023-02-23 04:35:22 +0800

    db

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -7,56 +7,50 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class CharacterController : ControllerBase
-	{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
 
-		private readonly ICharacterService _characterService;
+        private readonly ICharacterService _characterService;
 
-		public CharacterController(ICharacterService characterService)
-		{
-			_characterService = characterService;
-		}
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
-		[HttpGet("GetAll")]
-		public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
-		{
-			return Ok(await _characterService.GetAllCharacters());
-		}
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
 
-		[HttpGet("{id}")]
-		public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
-		{
-			return Ok(await _characterService.GetCharacterById(id));
-		}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
 
-		[HttpPost]
-		public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
-		{
-			return Ok(await _characterService.AddCharacter(newCharacter));
-		}
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
 
-		[HttpPut]
-		public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
-		{
-			var response = await _characterService.UpdateCharacter(updatedCharacter);
-			if (response.Data is null)
-			{
-				return NotFound(response);
-			}
-			return Ok(response);
-		}
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        {
+            var response = await _characterService.UpdateCharacter(updatedCharacter);
+            if (response.Data is null) return NotFound(response);
+            return Ok(response);
+        }
 
-		[HttpDelete("{id}")]
-		public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> DeleteCharacter(int id)
-		{
-			var response = await _characterService.DeleteCharacter(id);
-			if (response.Data is null)
-			{
-				return NotFound(response);
-			}
-			return Ok(response);
-		}
-	}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var response = await _characterService.DeleteCharacter(id);
+            if (response.Data is null) return NotFound(response);
+            return Ok(response);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@ global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,8 +22,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
